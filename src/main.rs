b992a3dab0860ab855ebb1db46c562648b451eaa
Clem Morton <Clem16@gmail.com>
2023-03-24 22:19:42 -0600

    Added retrieval of the date as well.

--- src/main.rs
+++ src/main.rs
@@ -4,14 +4,21 @@ use reqwest;
 async fn main() -> Result<(), Box<dyn std::error::Error>> {
     let url = "https://apc-cap.ic.gc.ca/datafiles/amateur_delim.zip";
     
-    // Make a GET request to the server and retrieve the ETag header
+    // Make a GET request to the server and retrieve the ETag and Last-Modified headers
     let response = reqwest::get(url).await?;
     let etag = response.headers().get("ETag");
+    let last_modified = response.headers().get("Last-Modified");
     
     if let Some(etag_value) = etag {
         println!("The ETag of {} is {}", url, etag_value.to_str()?);
     } else {
         println!("The ETag of {} could not be retrieved", url);
+    }
+    
+    if let Some(last_modified_value) = last_modified {
+        println!("The Last-Modified header of {} is {}", url, last_modified_value.to_str()?);
+    } else {
+        println!("The Last-Modified header of {} could not be retrieved", url);
     }
     
     Ok(())
